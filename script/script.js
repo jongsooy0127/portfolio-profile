9aeb688ad04060f5559ae1d5571d986b7d50fcb0
jongsooy0127 <77057480+jongsooy0127@users.noreply.github.com>
2024-07-19 18:00:07 +0900

    Edit assets and added type js

---
+++ script/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#element', {
+    strings: ['Web Developer', 'Full Stack Developer', 'Front-end Developer'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true
+});
